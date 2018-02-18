1e868bf7af04db87416790efc254a8057474b73f
Daria Marczak <marczak.ria@gmail.com>
2018-02-18 18:45:05 +0100

    Added projects showcase

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function initMap() {
-    var poznan = {lat: 52.4064, lng: 16.9252};
-    var map = new google.maps.Map(document.getElementById("mapGoogle"), {
-      zoom: 14,
-      center: poznan
-    });
-    var marker = new google.maps.Marker({
-      position: poznan,
-      map: map
-    });
-  }
+  var poznan = {
+    lat: 52.4064,
+    lng: 16.9252
+  };
+  var map = new google.maps.Map(document.getElementById("mapGoogle"), {
+    zoom: 14,
+    center: poznan
+  });
+  var marker = new google.maps.Marker({
+    position: poznan,
+    map: map
+  });
+}
